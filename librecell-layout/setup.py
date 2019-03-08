78c677eca0f93cb68f8539d728eeecc677970551
Thomas Kramer <dont@spam.me>
2019-03-08 13:15:59 +0100

    Bump version.

--- librecell-layout/setup.py
+++ librecell-layout/setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='librecell-layout',
-      version='0.0.3',
+      version='0.0.4',
       description='CMOS standard cell generator.',
       long_description=readme(),
       long_description_content_type="text/markdown",
