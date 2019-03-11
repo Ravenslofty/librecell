73c704f23bd8bdfd8c0408e6b56781d6597c8338
Thomas Kramer <dont@spam.me>
2019-03-11 22:36:11 +0100

    :scroll: Change license to OHL-S v2.0.

---
+++ librecell-layout/lclayout/drc_cleaner/__init__.py
@@ -0,0 +1,21 @@
+##
+## Copyright (c) 2019 Thomas Kramer.
+## 
+## This file is part of librecell-layout 
+## (see https://codeberg.org/tok/librecell/src/branch/master/librecell-layout).
+## 
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the CERN Open Hardware License (CERN OHL-S) as it will be published
+## by the CERN, either version 2.0 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## CERN Open Hardware License for more details.
+## 
+## You should have received a copy of the CERN Open Hardware License
+## along with this program. If not, see <http://ohwr.org/licenses/>.
+## 
+## 
+##
--- librecell-layout/lclayout/graphrouter/hv_router.py
+++ librecell-layout/lclayout/graphrouter/hv_router.py
@@ -5,8 +5,8 @@
 ## (see https://codeberg.org/tok/librecell/src/branch/master/librecell-layout).
 ## 
 ## This program is free software: you can redistribute it and/or modify
-## it under the terms of the CERN Open Hardware License (CERN OHL) as published by
-## the CERN, either version 1.2 of the License, or
+## it under the terms of the CERN Open Hardware License (CERN OHL-S) as it will be published
+## by the CERN, either version 2.0 of the License, or
 ## (at your option) any later version.
 ## 
 ## This program is distributed in the hope that it will be useful,
@@ -16,6 +16,8 @@
 ## 
 ## You should have received a copy of the CERN Open Hardware License
 ## along with this program. If not, see <http://ohwr.org/licenses/>.
+## 
+## 
 ##
 import networkx as nx
 from itertools import chain, combinations, product
