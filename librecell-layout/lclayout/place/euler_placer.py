116ba7193daaa9faa86b22580ba80bb12da65ae7
Thomas Kramer <dont@spam.me>
2019-06-29 11:41:14 +0200

    HierarchicalPlacer: Break symmetry by preferring input nets to be on the left side.

--- librecell-layout/lclayout/place/euler_placer.py
+++ librecell-layout/lclayout/place/euler_placer.py
@@ -116,17 +116,36 @@ def _assemble_cell(lower_row: List[Transistor], upper_row: List[Transistor]) -> 
     return cell
 
 
-def _io_ordering_cost(row: List[Transistor], input_nets: Set[Any], output_nets: Set[Any]) -> float:
+def _row_io_ordering_cost(row: List[Transistor], input_nets: Set[Any], output_nets: Set[Any]) -> float:
     """ Return a low cost if input nets are placed on the left side and outputs on the right side.
     """
 
-    w = len(row)
+    nets = [(net, pos) for pos, net in
+            enumerate(chain(*[[t.left, t.gate, t.right] for t in row if t is not None]))
+            ]
+    return _io_ordering_cost(nets, input_nets, output_nets)
+
+
+def _io_ordering_cost(nets: Iterable[Tuple[Hashable, float]], input_nets: Set[Any], output_nets: Set[Any]) -> float:
+    """ Return a low cost if input nets are placed on the left side and outputs on the right side.
+    :param nets: Tuples of (net name, x-position)
+    :param input_nets: Names of input nets.
+    :param output_nets: Names of output nets.
+    :return: Returns a cost which prefers that input nets are placed on the left and output nets are placed on the right.
+    """
+
+    nets = list(nets)
+
     cost = 0
-    for pos, net in enumerate(chain(*[[t.left, t.gate, t.right] for t in row if t is not None])):
+    min_x = min((pos for _, pos in nets))
+    max_x = max((pos for _, pos in nets))
+
+    for net, pos in nets:
         if net in input_nets:
-            cost += pos
+            cost += pos - min_x
         elif net in output_nets:
-            cost += w - pos
+            cost += max_x - pos
+
     return cost
 
 
@@ -152,9 +171,9 @@ def _cell_quality(cell: Cell, input_nets, output_nets):
     :param output_nets:
     :return:
     """
-    return _num_gate_matches(cell), -wiring_length_bbox(cell), - _io_ordering_cost(cell.upper, input_nets,
-                                                                                   output_nets) - \
-           _io_ordering_cost(cell.lower, input_nets, output_nets)
+    return _num_gate_matches(cell), -wiring_length_bbox(cell), \
+           - _row_io_ordering_cost(cell.upper, input_nets, output_nets) \
+           - _row_io_ordering_cost(cell.lower, input_nets, output_nets)
 
 
 def __need_gap(left: Transistor, right: Transistor):
@@ -206,15 +225,6 @@ def wiring_length_bbox(cell: Cell) -> float:
     The bounding box size of the nets is used as an approximation.
     """
 
-    # TODO use _wiring_length_bbox1
-    # Find for each net its leftmost (min) and rightmost (max) occurrence.
-    def find_all_bbox_x(row, minmax_x):
-        for pos, net in enumerate(chain(*[[t.left, t.gate, t.right] for t in row if t is not None])):
-            minmax_x[net] = (
-                min(pos, minmax_x.get(net, (pos, pos))[0]),
-                max(pos, minmax_x.get(net, (pos, pos))[1])
-            )
-
     net_positions = []
     for row in (cell.upper, cell.lower):
         for pos, net in enumerate(chain(*[[t.left, t.gate, t.right] for t in row if t is not None])):
@@ -235,6 +245,7 @@ class HierarchicalPlacer(TransistorPlacer):
         :param transistors:
         :return:
         """
+
         # Split into nmos / pmos
         nmos = [t for t in transistors if t.channel_type == ChannelType.NMOS]
         pmos = [t for t in transistors if t.channel_type == ChannelType.PMOS]
@@ -251,6 +262,7 @@ class HierarchicalPlacer(TransistorPlacer):
             """
             assert len(g) > 0
             placements = _find_optimal_single_row_placements(g)
+            # Append diffusion gap.
             placements = [list(chain(p, [None])) for p in placements]
             return placements
 
@@ -327,17 +339,23 @@ class HierarchicalPlacer(TransistorPlacer):
         best_subcell_placements = []
         best_wiring_length = None
 
+        # Extract input nets.
+        input_nets = net_util.get_cell_inputs(transistors)
+        output_nets = {} # TODO: extract output nets.
+
         for ps, ns in subcell_placements:
             ppos = get_subcell_net_position(ps)
             npos = get_subcell_net_position(ns)
 
             # TODO: make quality metric more flexible.
-            wiring_length = _wiring_length_bbox1(chain(ppos, npos))
 
-            if best_wiring_length is None or wiring_length < best_wiring_length:
+            wiring_length = _wiring_length_bbox1(chain(ppos, npos))
+            cost = wiring_length, _io_ordering_cost(chain(ppos, npos), input_nets, output_nets)
+
+            if best_wiring_length is None or cost < best_wiring_length:
                 best_subcell_placements.clear()
-                best_wiring_length = wiring_length
-            if wiring_length <= best_wiring_length:
+                best_wiring_length = cost
+            if cost <= best_wiring_length:
                 best_subcell_placements.append((ps, ns))
 
         logger.debug('Number of best sub cell placements: %d', len(best_subcell_placements))
@@ -558,6 +576,7 @@ def _find_optimal_single_row_placements(transistor_graph: nx.MultiGraph) -> List
         # assert len(_optimal) == len(_optimal_set)
         # Deduplicate
         optimal = list(_optimal_set)
+
         return optimal
 
     def edges2transistors(edges: Tuple[Any, Any, int]) -> List[Transistor]:
@@ -631,8 +650,8 @@ class EulerPlacer(TransistorPlacer):
         best_cells_wiring = all_min(best_cells_gate_match, key=wiring_length_bbox)
 
         def io_ordering_cost(cell: Cell):
-            return _io_ordering_cost(cell.lower, input_nets, output_nets) + \
-                   _io_ordering_cost(cell.upper, input_nets, output_nets)
+            return _row_io_ordering_cost(cell.lower, input_nets, output_nets) + \
+                   _row_io_ordering_cost(cell.upper, input_nets, output_nets)
 
         best_cells_io_ordering = all_min(best_cells_wiring, key=io_ordering_cost)
         best_cells = best_cells_io_ordering
