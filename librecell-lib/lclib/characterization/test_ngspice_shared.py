ef17ccb2f5d607830135bc12c8b71657cc435ed3
Thomas Kramer <code@tkramer.ch>
2020-06-27 20:04:11 +0200

    Example for using NgspiceShared directly.

--- librecell-lib/lclib/characterization/test_ngspice_shared.py
+++ librecell-lib/lclib/characterization/test_ngspice_shared.py
@@ -27,40 +27,100 @@ import PySpice.Logging.Logging as Logging
 import numpy as np
 
 from .ngspice_simulation import piece_wise_linear_voltage_source
-from .util import PieceWiseLinear
+from .piece_wise_linear import PieceWiseLinear
+
+import matplotlib.pyplot as plt
 
 
-def test_simulate_circuit():
-    time_step = 1 @ u_ms
-    end_time = 10 @ u_s
+# def test_simulate_circuit():
+#     time_step = 1 @ u_ms
+#     end_time = 10 @ u_s
+#
+#     circuit = Circuit(title='test1')
+#
+#     # circuit.V('VDD', 'vdd', circuit.gnd, 10 @ u_V)
+#     circuit.R('1', 'input', 'a', 1 @ u_kOhm)
+#     circuit.C('1', circuit.gnd, 'a', 1000 @ u_uF)
+#
+#     ngspice_shared = NgSpiceShared.new_instance(send_data=False)
+#
+#     piece_wise_linear_voltage_source(circuit, 'src', 'input', circuit.gnd,
+#                                      PieceWiseLinear([0, 1, 2, 10], [0, 1, 1, 1]))
+#
+#     # simulator = circuit.simulator(temperature=25,
+#     #                               nominal_temperature=25,
+#     #                               simulator='ngspice-shared',
+#     #                               ngspice_shared=ngspice_shared)
+#
+#     ngspice_shared.stop('v(a) > 0.5')
+#     # ngspice_shared.destroy()
+#     # print(str(simulator))
+#     # ngspice_shared.load_circuit(str(simulator))
+#     # ngspice_shared.run()
+#     # analysis = ngspice_shared.plot(simulator, 'asdf').to_analysis()
+#
+#     simulator = circuit.simulator(temperature=25,
+#                                   nominal_temperature=25,
+#                                   simulator='ngspice-shared',
+#                                   ngspice_shared=ngspice_shared)
+#     analysis = simulator.transient(step_time=time_step, end_time=end_time)
+#
+#     print(np.array(analysis['a']))
+#
+#     # plt.plot(analysis['a'])
+#     # plt.plot(analysis['input'])
+#     # plt.show()
 
-    circuit = Circuit(title='test')
 
-    circuit.V('VDD', 'vdd', circuit.gnd, 10 @ u_V)
+def test_simulate_circuit_lowlevel_control():
+    """
+    Use `NgSpiceShared` to get finer grained control over the simulation.
+    Especially needed for breakpoints.
+    """
+    circuit = Circuit(title='test2')
+
     circuit.R('1', 'input', 'a', 1 @ u_kOhm)
-    circuit.C('1', circuit.gnd, 'a', 1 @ u_nF)
+    circuit.C('1', circuit.gnd, 'a', 1000 @ u_uF)
 
-    ngspice_shared = NgSpiceShared.new_instance(send_data=False)
+    # piece_wise_linear_voltage_source(circuit, 'src', 'input', circuit.gnd,
+    #                                  PieceWiseLinear([0, 1, 2, 10], [0, 1, 1, 0]))
+    circuit.V('src', 'input', circuit.gnd, 0)
 
-    piece_wise_linear_voltage_source(circuit, 'src', circuit.gnd, 'input',
-                                     PieceWiseLinear([0, 1, 2, 10], [0, 1, 1, 1]))
+    ngs = NgSpiceShared.new_instance(send_data=False)
 
-    simulator = circuit.simulator(temperature=25,
-                                  nominal_temperature=25,
-                                  simulator='ngspice-shared',
-                                  ngspice_shared=ngspice_shared)
 
-    ngspice_shared.stop('v(a) > 10')
-    # ngspice_shared.destroy()
-    # print(str(simulator))
-    # ngspice_shared.load_circuit(str(simulator))
-    # ngspice_shared.run()
-    # analysis = ngspice_shared.plot(simulator, 'asdf').to_analysis()
+    ngs.destroy()
 
-    simulator = circuit.simulator(temperature=25,
-                                  nominal_temperature=25,
-                                  simulator='ngspice-shared',
-                                  ngspice_shared=ngspice_shared)
-    analysis = simulator.transient(step_time=time_step, end_time=end_time)
+    print(str(circuit))
+    ngs.load_circuit(str(circuit) + "\n .end")
+    # Change capacitance.
+    # ngs.alter_device('C1', C="1000uF")
+    # Change signal of piece wise linar voltage source..
+    wave = PieceWiseLinear([0, 1, 10], [0, 1, 1])
+    pwl_string = ' '.join((
+        '%es %eV' % (float(time), float(voltage))
+        for time, voltage in zip(wave.x, wave.y)
+    ))
+    ngs.alter_device('Vsrc', PWL="({})".format(pwl_string))
+
+    # Set temperature.
+    ngs.option(temp=25)
+    ngs.option(nomtemp=25)
+    # Set breakpoint.
+    ngs.stop('v(a) > 0.9')
+    # Run simulation.
+    ngs.exec_command("tran 1ms 60s")
+    ngs.run(background=False)
+
+    # Retreive signals.
+    plot_name = ngs.last_plot
+    print(plot_name)
+    if plot_name == 'const':
+        raise Exception("Simulation failed.")
+    plot = ngs.plot(None, plot_name)
+    analysis = plot.to_analysis()
 
     print(np.array(analysis['a']))
+    plt.plot(analysis['input'])
+    plt.plot(np.array(analysis['a']))
+    plt.show()
