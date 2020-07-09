2eb072c8354b272c9b65b52335d9ad291252bc95
Thomas Kramer <code@tkramer.ch>
2020-07-09 19:05:07 +0200

    :scroll: Update year in copyright notice. Update to CERN OHL-S v2.

---
+++ librecell-layout/lclayout/lvs/__init__.py
@@ -0,0 +1,13 @@
+#
+# Copyright 2019-2020 Thomas Kramer.
+#
+# This source describes Open Hardware and is licensed under the CERN-OHL-S v2.
+#
+# You may redistribute and modify this documentation and make products using it
+# under the terms of the CERN-OHL-S v2 (https:/cern.ch/cern-ohl).
+# This documentation is distributed WITHOUT ANY EXPRESS OR IMPLIED WARRANTY,
+# INCLUDING OF MERCHANTABILITY, SATISFACTORY QUALITY AND FITNESS FOR A PARTICULAR PURPOSE.
+# Please see the CERN-OHL-S v2 for applicable conditions.
+#
+# Source location: https://codeberg.org/tok/librecell
+#
