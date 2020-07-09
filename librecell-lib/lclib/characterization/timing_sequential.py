32de21efa6dbe24b2a38bcb787cd6c64c90229aa
Thomas Kramer <code@tkramer.ch>
2020-07-09 20:58:18 +0200

    Add imports that were lost during the merge.

--- librecell-lib/lclib/characterization/timing_sequential.py
+++ librecell-lib/lclib/characterization/timing_sequential.py
@@ -17,6 +17,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
+import tempfile
+import matplotlib.pyplot as plt
 from itertools import count
 
 from .ngspice_subprocess import run_simulation
