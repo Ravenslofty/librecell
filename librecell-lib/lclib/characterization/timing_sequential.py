19058253f7af665efe5f0796370d47d5865a376a
Thomas Kramer <code@tkramer.ch>
2020-07-09 19:10:15 +0200

    Remove unused imports.

--- librecell-lib/lclib/characterization/timing_sequential.py
+++ librecell-lib/lclib/characterization/timing_sequential.py
@@ -17,12 +17,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-from PySpice.Spice.Netlist import Circuit
-from PySpice.Unit import *
-from PySpice.Unit.SiUnits import Farad, Second
-
-import PySpice.Logging.Logging as Logging
-
 from itertools import count
 
 from .ngspice_subprocess import run_simulation
