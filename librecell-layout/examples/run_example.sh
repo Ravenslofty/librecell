14e31d9e1ea35058666af76ce6a3091f470b97ea
Thomas Kramer <code@tkramer.ch>
2021-03-26 17:15:49 +0100

    Fix typo.

--- librecell-layout/examples/run_example.sh
+++ librecell-layout/examples/run_example.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir mylibrary
-# Generate the layout of an AND2X1 cewll.
+# Generate the layout of an AND2X1 cell.
 lclayout --output-dir mylibrary --tech dummy_tech.py --netlist cells.sp --cell AND2X1 --verbose
